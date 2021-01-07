81cd23cdd259e35a89a1b940a3e498e4ad9d8fc1
Brian Evans <ebrian101@gmail.com>
2021-01-07 20:21:37 +0000

    Added prettier
    Signed-off-by: Brian Evans <ebrian101@gmail.com>

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
 
-console.log('Hello World from your main file!');
-ReactDOM.render(<h1>Frontend</h1>, document.getElementById("root"))
+console.log('Hello World from your main file!')
+ReactDOM.render(<h1>Frontend</h1>, document.getElementById('root'))
