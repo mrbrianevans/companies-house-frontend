5c6ab1d9f73c05647c724960796c4dce4490d3f5
Brian Evans <ebrian101@gmail.com>
2021-03-26 13:02:02 +0000

    Added tests for cloud functions
    Signed-off-by: Brian Evans <ebrian101@gmail.com>

--- apiSpeedTests.js
+++ apiSpeedTests.js
@@ -11,6 +11,10 @@ const runSpeedTest = async (companyNumbers, baseUrl, config, urlSuffix) => {
       const companyNumber = companyNumbers[i].number;
       try {
         // console.time(`Call API on ${companyNumber}`);
+        // pause for a second if using cloud function due to max instances
+        // if(baseUrl.toString().startsWith('https://europe-west1')){
+        //   await new Promise((resolve => setTimeout(resolve, 1000)))
+        // }
         const startTime = Date.now();
         const res = await axios.get(`${baseUrl}${companyNumber}${urlSuffix || ""}`, config || {});
         if (res.status !== 200) throw new ApiError(res.status, res.statusText);
@@ -66,27 +70,36 @@ const runComparisonSpeedTests = async (qty) => {
   const dev = "http://localhost:8080/api/company/";
   const production = "http://companies-house-frontend-api-rmfuc.ondigitalocean.app/api/company/";
   const brianevans = "https://brianevans.tech/projects/companies-house/database/controller.php?action=company-api&number=";
+  const nodeCloudFunction = "https://europe-west1-companies-house-data.cloudfunctions.net/getCompanyInfo?company_number=";
+  const goCloudFunction = "https://europe-west1-companies-house-data.cloudfunctions.net/getCompanyInfoGo?company_number=";
+  const pythonCloudFunction = "https://europe-west1-companies-house-data.cloudfunctions.net/getCompanyInfoPython?company_number=";
 
   const companiesHouseApiKey = "dB7bNOkkBwiC3TB8R-QfcLzWuCzP-gPT-sXAtuP1";
   const companiesHouseApiAuthed = "https://api.company-information.service.gov.uk/company/";
 
   // await runSpeedTest(companyNumbers, companiesHouseAPI, {}, ".json");
-  const companiesHouseAPIResponseTimes = await runSpeedTest(companyNumbers, companiesHouseApiAuthed, {
-    auth: {
-      username: companiesHouseApiKey,
-      password: ""
-    }
-  });
-  const localHostResponseTimes = await runSpeedTest(companyNumbers, dev);
-  const productionResponseTimes = await runSpeedTest(companyNumbers, production);
-  const brianEvansTechResponseTimes = await runSpeedTest(companyNumbers, brianevans);
-  const results = [
-    companiesHouseAPIResponseTimes,
-    localHostResponseTimes,
-    productionResponseTimes,
-    brianEvansTechResponseTimes
-  ];
-  fs.writeFileSync("speed-results/" + Date.now() + ".json", JSON.stringify(results, null, 2));
+  // const companiesHouseAPIResponseTimes = await runSpeedTest(companyNumbers, companiesHouseApiAuthed, {
+  //   auth: {
+  //     username: companiesHouseApiKey,
+  //     password: ""
+  //   }
+  // });
+  // const localHostResponseTimes = await runSpeedTest(companyNumbers, dev);
+  // const productionResponseTimes = await runSpeedTest(companyNumbers, production);
+  // const brianEvansTechResponseTimes = await runSpeedTest(companyNumbers, brianevans);
+  const pythonCloudFunctionResponseTimes = await runSpeedTest(companyNumbers, pythonCloudFunction);
+  const cloudFunctionResponseTimes = await runSpeedTest(companyNumbers, nodeCloudFunction);
+  const goCloudFunctionResponseTimes = await runSpeedTest(companyNumbers, goCloudFunction);
+
+  // const results = [
+  //   companiesHouseAPIResponseTimes,
+  //   // localHostResponseTimes,
+  //   // productionResponseTimes,
+  //   brianEvansTechResponseTimes,
+  //   cloudFunctionResponseTimes,
+  //   goCloudFunctionResponseTimes
+  // ];
+  // fs.writeFileSync("speed-results/" + Date.now() + ".json", JSON.stringify(results, null, 2));
   // console.log(results);
 };
 
