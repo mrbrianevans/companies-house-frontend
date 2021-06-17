a0e3781703504029ee1c5ee9d1cc0b4ae6dd9ec2
Brian Evans <ebrian101@gmail.com>
2021-06-17 20:52:07 +0100

    Write env file before testing from env variable
    Signed-off-by: Brian Evans <ebrian101@gmail.com>

--- helpers/connectToDatabase.ts
+++ helpers/connectToDatabase.ts
@@ -2,7 +2,7 @@ import { Pool } from 'pg'
 type GetDatabasePoolParams = {
   timeout_milliseconds?: number
 }
-export const getDatabasePool: (params?: GetDatabasePoolParams) => Pool = ({ timeout_milliseconds }) => {
+export const getDatabasePool: (params?: GetDatabasePoolParams) => Pool = (params) => {
   return new Pool({
     host: process.env.CLOUD_SQL_CONNECTION_NAME
       ? `${process.env.DB_SOCKET_PATH || '/cloudsql'}/${process.env.CLOUD_SQL_CONNECTION_NAME}`
@@ -11,6 +11,6 @@ export const getDatabasePool: (params?: GetDatabasePoolParams) => Pool = ({ time
     password: process.env.PGPASSWORD,
     database: process.env.PGDATABASE,
     port: Number(process.env.PGPORT),
-    statement_timeout: timeout_milliseconds ?? 60_000 // timeout statements after a while (milliseconds)
+    statement_timeout: params?.timeout_milliseconds ?? 60_000 // timeout statements after a while (milliseconds)
   })
 }
--- tests/browser/globalSetup.ts
+++ tests/browser/globalSetup.ts
@@ -7,6 +7,10 @@ export default async function globalSetup() {
   // creates a temporary directory prefixed with output to save screenshots and JSON before they get persisted
   process.env.BROWSER_TEST_OUTPUT_DIR = fs.mkdtempSync('tests/browser/output')
   process.env.PLAYWRIGHT_JSON_OUTPUT_NAME = `${process.env.BROWSER_TEST_OUTPUT_DIR}/testResults.json`
+  fs.writeFileSync(
+    '.env.production.local',
+    Buffer.from(process.env.ENV_PRODUCTION_LOCAL_FILE, 'base64').toString('utf8')
+  )
   if (Number(process.env.SKIP_BUILD) === 1) {
     console.log('Skipping build as SKIP_BUILD is set')
   } else {
